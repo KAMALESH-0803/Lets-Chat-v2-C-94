735dcd7f82277890d10102f59edaea9ac9b28cf7
KAMALESH-0803 <89772906+KAMALESH-0803@users.noreply.github.com>
2022-03-30 16:13:44 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCkOtA2LUdBrYiSto0It2Bpr_qnBpv00DM",
+    authDomain: "lets-chat-7af9e.firebaseapp.com",
+    databaseURL: "https://lets-chat-7af9e-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-7af9e",
+    storageBucket: "lets-chat-7af9e.appspot.com",
+    messagingSenderId: "402514253024",
+    appId: "1:402514253024:web:55e256d10bd7ac50abac85"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
